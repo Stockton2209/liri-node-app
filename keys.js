3df2266d98c79316c0f1cd0a77f6e88acc1cbdd4
Alexander <alexstockton09@gmail.com>
2018-11-25 12:54:54 -0500

    created the repo, initial installs and files

---
+++ keys.js
@@ -0,0 +1,10 @@
+js
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+//spotify client id: 03dd8d402b8e484daa889d011168393f
+//spotify client secret: 37bcf89de0f64492874ff856ef4565a5
